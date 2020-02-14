8d8d08bbe0e74ac4eeea44451e25bad1a70f5ccc
RA <ra_lebedev@mail.ru>
2020-02-14 11:12:37 +0600

    !

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -3,26 +3,36 @@
 
 namespace app\controllers;
 
+use yii\base\ErrorException;
 use yii\httpclient\Client;
+use yii\httpclient\Exception;
 use yii\web\Controller;
+use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
 
 class TestController extends Controller
 {
     public function actionIndex()
     {
-        $client = new Client([
-            'baseUrl' => 'https://localhost:44387/api/',
-            'requestConfig' => [
-                'format' => Client::FORMAT_JSON,
-            ],
-            'responseConfig' => [
-                'format' => Client::FORMAT_JSON,
-            ]
-        ]);
-        $response = $client->createRequest()
-            ->setMethod('get')
-            ->setUrl('TimingTrainings/')
-            ->send();
+        try {
+
+            $client = new Client([
+                'baseUrl' => 'https://localhost:44387/api/',
+                'requestConfig' => [
+                    'format' => Client::FORMAT_JSON,
+                ],
+                'responseConfig' => [
+                    'format' => Client::FORMAT_JSON,
+                ]
+            ]);
+            $response = $client->createRequest()
+                ->setMethod('get')
+                ->setUrl('TimingTrainings/')
+                ->send();
+        } catch (\Exception $exception) {
+            throw new Exception('Сервер не доступен', $exception->getCode());
+        }
+
 
         $content = json_decode($response->getContent());
         if($response->isOk){
