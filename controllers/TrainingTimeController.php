8b9d7250de110eb05fd7c724552851fa9b120aff
RA <ra_lebedev@mail.ru>
2020-02-14 16:51:54 +0600

    !

--- controllers/TrainingTimeController.php
+++ controllers/TrainingTimeController.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 namespace app\controllers;
 
 use yii\base\ErrorException;
@@ -10,8 +11,9 @@ use yii\web\Controller;
 use yii\web\HttpException;
 use yii\web\NotFoundHttpException;
 
-class TestController extends Controller
+class TrainingTimeController extends Controller
 {
+
     public function actionIndex()
     {
         try {
@@ -40,7 +42,34 @@ class TestController extends Controller
                 'content' => $content,
             ]);
         }
+    }
 
+    public function actionCreate()
+    {
+        $request = Yii::$app->request;
+        if ($request->isPost) {
+            
+        }
+        try {
+
+            $client = new Client([
+                'baseUrl' => 'https://localhost:44387/api/',
+                'requestConfig' => [
+                    'format' => Client::FORMAT_JSON,
+                ],
+                'responseConfig' => [
+                    'format' => Client::FORMAT_JSON,
+                ]
+            ]);
+            $response = $client->createRequest()
+                ->setMethod('get')
+                ->setUrl('TimingTrainings/')
+                ->send();
+        } catch (\Exception $exception) {
+            throw new Exception('Сервер не доступен', $exception->getCode());
+        }
+
+        $content = json_decode($response->getContent());
 
 
     }
---
+++ views/training-time/index.php
@@ -0,0 +1,55 @@
+<?php
+
+use \yii\helpers\ArrayHelper;
+/* @var $this yii\web\View */
+/* @var $content */
+?>
+
+
+<div class="row">
+    <div class="col-md-12">
+        <p>Расписание тренировок</p>
+        <a class="btn btn-success" href="create">Добавить</a>
+    </div>
+    <div class="col-md-12">
+
+        <table class="table table-striped">
+            <thead>
+            <tr>
+                <th>Номер зала</th>
+                <th>Вместимость</th>
+                <th>Статус</th>
+                <th>Тренер</th>
+                <th>Вид(инд/групп)</th>
+                <th>Кол-во участников</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach ($content as $item): ?>
+                <tr>
+                    <td>
+                        <?= ArrayHelper::getValue($item, 'Gym.Number') ?>
+                    </td>
+                    <td>
+                        <?= ArrayHelper::getValue($item, 'Gym.Capacity') ?>
+                    </td>
+                    <td>
+                        <?= ArrayHelper::getValue($item, 'Gym.Active') ?>
+                    </td>
+                    <td>
+                        <?= ArrayHelper::getValue($item, 'Coach.FIO') ?>
+                    </td>
+                    <td>
+                        <?= ArrayHelper::getValue($item, 'Type.TypeName') ?>
+                    </td>
+                    <td>
+                        <?= ArrayHelper::getValue($item, 'Quantity') ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+
+        </table>
+    </div>
+</div>
+
